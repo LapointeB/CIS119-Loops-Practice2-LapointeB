8348ba0d1bf20e7394629ac08a55eff8e4bf2cfa
LapointeB <LapointeB@T29685.niaccit.niacc.edu>
2020-10-13 09:55:04 -0500

    Wrote the program

--- public_html/LoopsPractice2.js
+++ public_html/LoopsPractice2.js
@@ -3,6 +3,17 @@
  * Keep a running total.
  * Display the final total.
  */
+let counter = 1;
+let sum = 0;
+let number = 0;
 
-document.write("This program asks the user for 5 numbers and prints their total.");
+document.write("This program asks the user for 5 numbers and prints their total. <br>");
+
+while (counter <= 5) {
+    number = Number(prompt("Enter number " + counter + ": "));
+    counter++;
+    sum = sum + number;
+}
+
+document.write("Your total is " + sum + ". ");
 
